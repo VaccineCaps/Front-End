16962dee08e9db52bc22b1ed15c8e007d85abda8
TarisRafiqi <taris.izatri18@student.uisi.ac.id>
2022-07-08 02:03:30 +0700

    Memperbaiki Halaman Menu Banner Iklan

--- src/views/MenuBannerIklan.vue
+++ src/views/MenuBannerIklan.vue
@@ -1,59 +1,58 @@
 <template>
-    <v-main class="main">
-        <v-app-bar id="bar" class="rounded-b-xl" elevation="3">
+  <v-main class="main">
+    <v-app-bar id="bar" class="rounded-b-xl" elevation="3">
       <h2 class="white--text font-weight-medium">Menu Banner Iklan</h2>
     </v-app-bar>
-    
-    <v-card class="rounded-xl ma-10 pa-10" color="#F1FDFF" elevation="3" height="850">
 
-        <v-layout row wrap justify-space-around>
-            <v-flex xs12 md12 lg5>
-                <v-card flat color="transparent">
-                    <v-title class="text-center"><h1>PREVIEW</h1></v-title>
-                    <v-img :src="url" height="300" contain class="grey "></v-img>
-                </v-card>
-            </v-flex>
+    <v-card class="pa-10 ma-10">
+      <v-layout wrap justify-space-around>
+        <v-flex xs12 md5>
+          <v-title class="text-center"><h1>PREVIEW</h1></v-title>
+          <v-img :src="url" height="400" contain class="grey"></v-img>
+        </v-flex>
 
-            <v-flex xs12 md12 lg5>
-                <v-card flat color="transparent">
-                    <h1>
-                        <br>
-                    </h1>
-                    <v-file-input 
-                    height="300"
-                    v-model="image" 
-                    accept=".png, .jpeg, .jpg" :rules="imagesize"
-                    hint="File extentions (.png, .jpg, .jpeg) File Size limits (max: 10mb)"
-                    placeholder ="Upload profile image"
-                    prepend-icon=""
-                    outlined
-                    filled
-                    @click: clear="url=''"
-                    @change="Preview_image">
-                </v-file-input>
-                </v-card>
-                
-            </v-flex>
-        </v-layout>
-        </v-card>
-        
-    </v-main>
+        <v-flex xs12 md5>
+          <v-icon large color="black"> mdi-upload </v-icon>
+          <v-file-input
+            height="400"
+            v-model="image"
+            accept=".png, .jpeg, .jpg"
+            :rules="imagesize"
+            hint="File extentions (.png, .jpg, .jpeg) File Size limits (max: 10mb)"
+            placeholder="Upload profile image"
+            prepend-icon=""
+            outlined
+            filled
+            @click:
+            clear="url=''"
+            @change="Preview_image"
+          >
+          </v-file-input>
+        </v-flex>
+      </v-layout>
+
+      <v-layout justify-center class="mt-10">
+        <v-btn depressed color="primary" x-large class="mr-3">Kembali</v-btn>
+        <v-btn depressed color="primary" x-large>Save</v-btn>
+      </v-layout>
+    </v-card>
+  </v-main>
 </template>
 
 <script>
 export default {
-    data() {
-        return{
-            url: ''
-        }
-    },
+  data() {
+    return {
+      url: "",
+    };
+  },
 
-    methods: {
-        Preview_image() {
-            this.url = URL.createObjectURL(this.image);
-        },
-    }
-}
+  methods: {
+    Preview_image() {
+      this.url = URL.createObjectURL(this.image);
+    },
+  },
+};
 </script>
 
 
