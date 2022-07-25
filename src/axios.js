d81920ea9436089815cb309c16295266637750dd
Dzikri Ardafi <99666120+Dzikri-Ardafi@users.noreply.github.com>
2022-07-26 05:21:31 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  "http://ec2-13-57-43-205.us-west-1.compute.amazonaws.com/admin/";
+  "http://103.171.85.200:2004/admin/";
 
 (axios.defaults.headers.common["Content-Type"] = "application/json"),
   (axios.defaults.headers.common["Authorization"] =
