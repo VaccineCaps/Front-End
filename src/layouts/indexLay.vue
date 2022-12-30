7bf830feb6842eef0040b91833964e8c4f7f9c85
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-12-30 16:35:27 +0700

    update

--- src/layouts/indexLay.vue
+++ src/layouts/indexLay.vue
@@ -1,14 +1,11 @@
 <template>
-<v-main style="margin-left:20px;">
+<v-main>
     <v-row>
     <v-col cols="2">
       <SideBar/>
     </v-col>
-    <v-col cols="10" class="" style="margin-top: 0px; margin-left: px;">
-        <!-- <v-container> -->
+    <v-col cols="10">
             <router-view></router-view>
-        <!-- </v-container> -->
-        
     </v-col>
     </v-row>
 </v-main>
